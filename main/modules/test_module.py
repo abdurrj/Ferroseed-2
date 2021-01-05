9f546d277a739cc90df98caee4370c02955b77a4
abdurrj <abdurrj@users.noreply.github.com>
2021-01-05 18:35:39 +0100

    Update test_module.py

--- main/modules/test_module.py
+++ main/modules/test_module.py
@@ -8,7 +8,7 @@ class test_module(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send(f"Command registered, latency: {str(round(self.client.latency, 3))}")
+        await ctx.send(f"Command registered, latency: {str(round(self.client.latency, 4))}")
         
 def setup(client):
     client.add_cog(test_module(client))
